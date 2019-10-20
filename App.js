432f06a1c0b8f8e2ca9fc4b5afbca3d5df62482f
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-10-19 22:58:24 -0400

    First commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <br></br><br></br><br></br>
+        <a href='https://developer.apple.com/documentation/' className='link-button'>
+        Apple Developer Documentation</a>
+        <a href='https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html' className='link-button'>
+        Swift Language Documentation</a>
+        <br></br><br></br><br></br>
+        <a href='https://www.w3schools.com/' className='link-button'>
+        w3schools</a>
+        <a href='https://developer.mozilla.org/en-US/' className='link-button'>
+        Mozilla Developer Network Documentation</a>
+        <br></br><br></br><br></br>
+        <a href='https://reactjs.org/tutorial/tutorial.html' className='link-button'>
+        React JS Tutorial</a>
+        <a href='https://github.com/facebook/create-react-app' className='link-button'>
+        Create React App</a>
+        <br></br><br></br><br></br>
+        <a href='https://docs.oracle.com/javase/tutorial/index.html' className='link-button'>
+        Oracle's Java Learning Tutorials</a>
+        <span className='text-on-background'>Java's docs for their api's can best be found on the oracle website through a search engine</span>
+        <br></br><br></br><br></br>
+        <a href='https://docs.oracle.com/javase/tutorial/java/index.html' className='link-button'>
+        Oracle's Learning the Java Language Tutorials</a>
+        <br></br><br></br><br></br>
+        <a href='https://dev.mysql.com/doc/refman/8.0/en/' className='link-button'>
+        MySQL reference manual</a>
+        <a href='https://dev.mysql.com/downloads/' className='link-button'>
+        MySQL downloads</a>
+    </div>
+  );
+}
+
+export default App;
