737e99ae2ac13a1926cb173ccaa6fa9166e0c47d
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-10-23 22:21:40 -0400

    Start on LinkSection

---
+++ LinksSectionModel.js
@@ -0,0 +1,9 @@
+class LinksSectionModel{
+    title
+    links=[]
+    constructor(title, links){
+        this.title=title;
+        this.links=links;
+    }
+}
+export default LinksSectionModel;
---
+++ LinksSections.js
@@ -0,0 +1,7 @@
+import {appleDocsLink, swiftLanguageLink, rayWenderlichLink, androidDocsLink, w3SchoolLink, 
+    mdnDocs, freeCodeCampLink, reactTutorialLink, geeksForGeeksLink, createReactAppLink,
+    oracleJavaTutorialsLink, oracleJavaLanguageLink, mySQLReferenceLink, mySQLDownloadsLink,
+    githubLink, stackOverflowLink, pythonLink, python3DocsLink, python3TutorialLink, 
+    python3LibraryLink} from './Links';
+
+export const appleAndSwift=new LinksSectionModel('Apple and Swift', [appleDocsLink, swiftLanguageLink]);
